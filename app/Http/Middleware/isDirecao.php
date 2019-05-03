22ca437c86a0627b6791c7fa9a1a1c0f433f0a97
fsilva <franciscosilva00@gmail.com>
2019-05-03 16:26:10 +0100

    Adicionei e configurei middlware isDirecao

---
+++ app/Http/Middleware/isDirecao.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+
+class isDirecao
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->user() && $request->user()->direcao == 1){
+            return $next($request);
+        }
+
+        throw new AccessDeniedException('Unauthorized.');
+    }
+}
