db278a51f7fdd4483b6d8b7519e64bf26bfbd823
RicardoPSaramago <50020759+RicardoPSaramago@users.noreply.github.com>
2019-05-03 15:33:57 +0100

    Criei ficheiros: add.blade.php, edit.blade.php e alterei add-edit.balde.php Criei a pasta shared no resources/views/%shared% que tem os ficheiros success e errors .blade.php para mostrar os erros e os sucessos.

---
+++ resources/views/aeronaves/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Editar Aeronave')
+
+@section('content')
+ 
+@if (count($errors) > 0)
+    @include('shared.errors')
+@endif
+<!-- só os socios da direção é que pode acrescentar, alterar ou remover aeronaves! -->
+
+<form action="{{route('users.update', $user)}}" method="post" class="form-group">
+    {{method_field('PUT')}}
+    @include('aeronaves.partials.add-edit')
+    <div class="form-group">
+        <button type="submit" class="btn btn-success" name="ok">Guardar</button>
+        <a type="submit" class="btn btn-default" name="cancel" href="{{route('aeronaves.index')}}">Cancelar</a>
+    </div>
+</form>
+@endsection
