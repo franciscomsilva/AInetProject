86ea2e3e39ada494bc3e9f17c6cdc2e8489082ed
franciscosilva800 <franciscosilva800@gmail.com>
2019-05-04 19:41:22 +0100

    Atualizei lista de rotas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::paginate(5);
+        $users = User::paginate(15);
         return view('users.list', compact( 'users'));
     }
     /**
