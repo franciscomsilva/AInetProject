e1c88e760c0712c5a6a16f6dedc5d795ea9b5690
RicardoPSaramago <50020759+RicardoPSaramago@users.noreply.github.com>
2019-05-20 21:52:36 +0100

    autorização para autorizar pilotos

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware(['ativo','auth','verified'])->group(function () {
     Route::get('/home', 'HomeController@index')->name('user.home');
 
     // Sócios!
+    #region socios
     Route::get('/socios', 'UserController@index')->name('user.index');
     Route::get('/socios/{user}','UserController@show')->name('user.show');
     Route::get('/socios/create', 'UserController@create')->name('user.create');
@@ -29,8 +30,10 @@ Route::middleware(['ativo','auth','verified'])->group(function () {
     Route::get('/socios/{user}/edit', 'UserController@edit')->name('user.edit');
     Route::put('/socios/{user}/edit', 'UserController@update')->name('user.update');
     Route::delete('/socios/{user}', 'UserController@delete')->name('user.delete');
+    #endregion socios
 
     // Aeronaves!
+    #region aeronaves
     Route::get('/aeronaves', 'AeronaveController@index')->name('aeronaves.index');
 
     Route::get('/aeronaves/create', 'AeronaveController@create')->name('aeronaves.create');
@@ -38,13 +41,16 @@ Route::middleware(['ativo','auth','verified'])->group(function () {
     Route::get('/aeronaves/{aeronave}/edit', 'AeronaveController@edit')->name('aeronaves.edit');
     Route::put('/aeronaves/{aeronave}', 'AeronaveController@update')->name('aeronaves.update');
     Route::delete('/aeronaves/{aeronave}', 'AeronaveController@destroy')->name('aeronaves.destroy');
+    
     Route::get('/aeronaves/{aeronave}/pilotos', 'AeronaveController@pilotosIndex')->name('aeronaves.pilotosIndex');
     Route::get('/aeronaves/{aeronave}/pilotos/nao-autorizados', 'AeronaveController@pilotosNaoAutorizadosIndex')->name('aeronaves.pilotosNaoAutorizadosIndex'); //nova rota para pilotos nao autorizados
     Route::post('/aeronaves/{aeronave}/pilotos/{piloto}', 'AeronaveController@autorizarPiloto')->name('aeronaves.autorizarPiloto');
     Route::delete('/aeronaves/{aeronave}/pilotos/{piloto}', 'AeronaveController@removerPiloto')->name('aeronaves.removerPiloto');
     Route::get('/aeronaves/{aeronave}/precos_tempos', 'AeronaveController@precos_temposIndex')->name('aeronaves.precos_temposIndex');
-    
+    #endregion aeronaves
+
     //Movimentos!
+    #region movimentos
     Route::get('/movimentos', 'MovimentoController@index')->name('movimentos.index');
     Route::get('/movimentos/{movimento}', 'MovimentoController@show')->name('movimentos.show');
     Route::get('/movimentos/{movimento}/edit','MovimentoController@edit')->name('movimentos.edit');
@@ -52,5 +58,6 @@ Route::middleware(['ativo','auth','verified'])->group(function () {
     Route::post('/movimentos', 'MovimentoController@store')->name('movimentos.store');
     Route::put('/movimentos/{movimento}', 'MovimentoController@update')->name('movimentos.update');
     Route::delete('/movimentos/{movimento}', 'MovimentoController@destroy')->name('movimentos.destroy');
+    #endregion movimentos
 });
 
