f0d5a33eda76ee7a2761cabb90e321c8c1e3eb8f
franciscosilva800 <franciscosilva800@gmail.com>
2019-05-21 15:32:53 +0100

    [24] Adiconei UserRequest para criar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,7 +44,7 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateUserRequest $request)
     {
         //
     }
