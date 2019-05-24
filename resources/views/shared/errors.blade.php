a9e3bc0c2c61e6d66f6e2a5246ff585d21c390c0
Francisco Silva <39302013+franciscosilva800@users.noreply.github.com>
2019-05-24 16:33:05 +0100

    Delete errors.blade.php

--- resources/views/shared/errors.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="alert alert-danger">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    @foreach($errors->all() as $error)
-        <li>{{$error}}</li>
-    @endforeach
-</div>
