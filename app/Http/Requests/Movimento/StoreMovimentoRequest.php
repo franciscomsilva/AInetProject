6826bfaab97624decfd264402c144ebf102cb70d
José Maria Marques Andrade <JoseMariaAndrade@users.noreply.github.com>
2019-05-31 16:03:46 +0100

    Rules

--- app/Http/Requests/Movimento/StoreMovimentoRequest.php
+++ app/Http/Requests/Movimento/StoreMovimentoRequest.php
@@ -31,6 +31,42 @@ class StoreMovimentoRequest extends FormRequest
     public function rules()
     {
         return [
-            'data' => 'required|date'];
+            'data' => 'required|date',
+            'hora_descolagem' => 'required|datetime',
+            'hora_aterragem' => 'required|datetime',
+            'aeronave' => 'required',
+            'num_diario' => 'required|numeric',
+            'num_servico' => 'required|numeric',
+            'piloto_id' => 'required|numeric',
+            'num_licenca_piloto' => 'required|numeric',
+            'validade_licenca_piloto' => 'required|date',
+            'tipo_licenca_piloto' => 'required',
+            'num_certificado_piloto' => 'required|numeric',
+            'validade_certificado_piloto' => 'required|date',
+            'classe_certificado_piloto' => 'required',
+            'natureza' => 'required|in:T,I,E',
+            'aerodromo_partida' => 'required',
+            'aerodromo_chegada' => 'required',
+            'num_aterragens' => 'required|numeric',
+            'num_descolagens' => 'required|numeric',
+            'num_pessoas' => 'required|numeric',
+            'conta_horas_inicio' => 'required|numeric',
+            'conta_horas_fim' => 'required|numeric',
+            'tempo_voo' => 'required|numeric',
+            'preco_voo' => 'required|numeric',
+            'modo_pagamento' => 'required|in:N,M,T,P',
+            'num_recibo' => 'required',
+            'observacoes' => 'nullable|string',
+            'confirmado' => 'between:0,1',
+            'tipo_instrucao' => 'nullable',
+            'instrutor_id' => 'nullable',
+            'num_licenca_instrutor' => 'nullable',
+            'validade_licenca_instrutor' => 'nullable',
+            'tipo_licenca_instrutor' => 'nullable',
+            'num_certificado_instrutor' => 'nullable',
+            'validade_certificado_instrutor' => 'nullable',
+            'classe_certificado_instrutor' => 'nullable',
+            'tipo_conflito' => 'nullable',
+            'justificacao_conflito' => 'nullable'];
     }
 }
